cd16ac5b5522f29301a2f847ea4409a94081c9a4
jecktor <jecktor@proton.me>
2024-03-16 13:21:37 -0600

    feat: added payments using stripe url

---
+++ mobile-app/app/home.tsx
@@ -0,0 +1,235 @@
+import { Stack, Link, router } from 'expo-router';
+import { onValue, ref, update } from 'firebase/database';
+import { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  ImageBackground,
+  Linking,
+  type ImageSourcePropType,
+} from 'react-native';
+
+import { useFirebaseUser } from '../hooks';
+import { auth, db } from '../utils/firebase';
+
+interface CardProps {
+  title: string;
+  desc: string;
+  action: string;
+  background: ImageSourcePropType;
+  onPress: () => void;
+}
+
+function Card({ title, desc, action, background, onPress }: CardProps) {
+  return (
+    <ImageBackground source={background} resizeMode="cover">
+      <View style={cardStyles.container}>
+        <Text style={cardStyles.title}>{title}</Text>
+        <Text style={cardStyles.desc}>{desc}</Text>
+        <TouchableOpacity style={cardStyles.button} onPress={onPress}>
+          <Text style={cardStyles.buttonText}>{action}</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+}
+
+interface Locker {
+  number: number;
+  hasItems: boolean;
+}
+
+export default function Page() {
+  const { user, subscription, lockerId, loading } = useFirebaseUser();
+  const [locker, setLocker] = useState<Locker>();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!subscription) router.replace('/plans');
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [subscription]);
+
+  useEffect(() => {
+    if (!lockerId) return;
+
+    const lockerRef = ref(db, `lockers/entries/${lockerId}`);
+    onValue(lockerRef, (snapshot) => {
+      setLocker(snapshot.val());
+    });
+  }, [lockerId]);
+
+  function handleOpenLocker() {
+    if (!locker) return;
+
+    update(ref(db, `lockers/entries/${lockerId}`), {
+      open: true,
+    });
+  }
+
+  function handleManageSubscription() {
+    fetch('http://192.168.100.97:3001/create-billing-portal-link', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ customerId: user?.uid }),
+    })
+      .then((res) => res.json())
+      .then(({ url }) => Linking.openURL(url))
+      .catch(console.error);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Stack.Screen
+          options={{
+            title: '',
+            headerLeft: () => <></>,
+            headerTransparent: true,
+            statusBarTranslucent: true,
+            gestureEnabled: false,
+          }}
+        />
+        {loading || !user || !subscription ? (
+          <Text style={styles.title}>Loading...</Text>
+        ) : (
+          <>
+            <View style={styles.mainContainer}>
+              <View style={styles.head}>
+                <Text style={styles.title}>Welcome, {user.displayName!.split(' ')[0]}</Text>
+                <TouchableOpacity onPress={() => auth.signOut()}>
+                  <Image source={require('../assets/logout.png')} />
+                </TouchableOpacity>
+              </View>
+              <Card
+                title={`${subscription.plan} plan`}
+                desc={`Your plan will ${subscription.cancelled ? 'end' : 'renew'} on ${new Date(
+                  subscription.currentPeriodEnd
+                ).toLocaleDateString('en-us', {
+                  year: 'numeric',
+                  month: 'short',
+                  day: 'numeric',
+                })}.`}
+                action="Manage subscription"
+                background={require('../assets/plan.png')}
+                onPress={handleManageSubscription}
+              />
+              {locker ? (
+                <Card
+                  title={`Locker #${locker.number}`}
+                  desc={locker.hasItems ? "Don't forget your belongings." : 'Nothing inside.'}
+                  action="Open locker"
+                  background={require('../assets/locker.png')}
+                  onPress={handleOpenLocker}
+                />
+              ) : (
+                <Card
+                  title="Locker rental"
+                  desc="Upgrade your plan to access a locker."
+                  action="Upgrade plan"
+                  background={require('../assets/locker.png')}
+                  onPress={() => router.push('/plans')}
+                />
+              )}
+            </View>
+            <Link href={{ pathname: '/code', params: { userId: user.uid } }} asChild>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Access code</Text>
+              </TouchableOpacity>
+            </Link>
+          </>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const cardStyles = StyleSheet.create({
+  container: {
+    padding: 24,
+    borderRadius: 6,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: '#E4E4E7',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  desc: {
+    fontSize: 16,
+    color: '#71717A',
+    marginBottom: 24,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#E4E4E7',
+    borderRadius: 6,
+    elevation: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  buttonText: {
+    color: '#18181B',
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+});
+
+const styles = StyleSheet.create({
+  head: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#18181B',
+    borderRadius: 6,
+    elevation: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    marginTop: 48,
+    padding: 24,
+  },
+  mainContainer: {
+    marginTop: 32,
+    gap: 32,
+  },
+  main: {
+    flex: 1,
+    maxWidth: 960,
+    marginHorizontal: 'auto',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
---
+++ mobile-app/app/plans.tsx
@@ -0,0 +1,196 @@
+import { Stack, router } from 'expo-router';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image, Linking } from 'react-native';
+
+import { useFirebaseUser } from '../hooks';
+
+interface CardProps {
+  title: string;
+  desc: string;
+  features: string[];
+  onPress: () => void;
+  selected?: boolean;
+}
+
+function Card({ title, desc, features, onPress, selected = false }: CardProps) {
+  return (
+    <TouchableOpacity
+      style={selected ? cardStyles.selected : cardStyles.container}
+      onPress={onPress}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={cardStyles.title}>{title}</Text>
+        {selected && <Image source={require('../assets/check.png')} />}
+      </View>
+      <Text style={cardStyles.desc}>{desc}</Text>
+      <View style={{ gap: 6 }}>
+        {features.map((feature, i) => (
+          <Text key={i} style={cardStyles.list}>
+            • {feature}
+          </Text>
+        ))}
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+export default function Page() {
+  const { user, subscription, loading } = useFirebaseUser();
+  const [plan, setPlan] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (subscription) router.replace('/home');
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [subscription]);
+
+  function handleCheckout(planId: number) {
+    fetch('http://192.168.100.97:3001/create-checkout-link', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        planId,
+        customerId: user?.uid,
+      }),
+    })
+      .then((res) => res.json())
+      .then(({ url }) => Linking.openURL(url))
+      .catch(console.error);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Stack.Screen
+          options={{
+            title: '',
+            headerLeft: () => <></>,
+            headerTransparent: true,
+            statusBarTranslucent: true,
+            gestureEnabled: false,
+          }}
+        />
+        {loading || !user || subscription ? (
+          <Text style={styles.title}>Loading...</Text>
+        ) : (
+          <>
+            <View style={styles.mainContainer}>
+              <Text style={styles.title}>Choose your plan</Text>
+              <Card
+                title="Basic plan"
+                desc="$20/month"
+                features={['Access to all gym facilities.']}
+                onPress={() => setPlan(0)}
+                selected={plan === 0}
+              />
+              <Card
+                title="Pro plan"
+                desc="$25/month"
+                features={['Access to all gym facilities.', 'Locker included.']}
+                onPress={() => setPlan(1)}
+                selected={plan === 1}
+              />
+            </View>
+            <TouchableOpacity style={styles.button} onPress={() => handleCheckout(plan)}>
+              <Text style={styles.buttonText}>Confirm</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const cardStyles = StyleSheet.create({
+  container: {
+    padding: 24,
+    borderRadius: 6,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: '#E4E4E7',
+  },
+  selected: {
+    padding: 24,
+    borderRadius: 6,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: '#18181B',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  desc: {
+    fontSize: 16,
+    color: '#71717A',
+    marginBottom: 24,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#E4E4E7',
+    borderRadius: 6,
+    elevation: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  buttonText: {
+    color: '#18181B',
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+  list: {
+    fontSize: 16,
+    color: '#71717A',
+    marginLeft: 8,
+  },
+});
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#18181B',
+    borderRadius: 6,
+    elevation: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    marginTop: 48,
+    padding: 24,
+  },
+  mainContainer: {
+    marginTop: 32,
+    gap: 32,
+  },
+  main: {
+    flex: 1,
+    maxWidth: 960,
+    marginHorizontal: 'auto',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
