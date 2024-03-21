22ca8d0faa2abcc7190ee467e2d41e6c7c50195b
jecktor <jecktor@proton.me>
2024-03-20 19:24:44 -0600

    feat: added theme toggle

--- admin-dashboard/src/App.tsx
+++ admin-dashboard/src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider, ModeToggle } from "@components";
 import { Button } from "@components/ui/button";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider>
       <h1>Vite + React</h1>
       <Button>Test</Button>
-    </>
+      <ModeToggle />
+    </ThemeProvider>
   );
 }
---
+++ admin-dashboard/src/components/index.ts
@@ -0,0 +1,2 @@
+export { ThemeProvider } from "./ThemeProvider";
+export { ModeToggle } from "./ModeToggle";
