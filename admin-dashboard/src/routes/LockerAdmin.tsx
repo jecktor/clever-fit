37e0621291f9d9c63f660eca6c57c863a3898fe5
jecktor <jecktor@proton.me>
2024-03-24 15:24:49 -0600

    feat: tables

--- admin-dashboard/src/routes/LockerAdmin.tsx
+++ admin-dashboard/src/routes/LockerAdmin.tsx
@@ -3,7 +3,14 @@ import { Layout } from "@components";
 export function LockerAdmin() {
   return (
     <Layout>
-      <h1>LockerAdmin</h1>
+      <h1 className="text-[1.875rem] font-semibold leading-none">
+        Locker Administration
+      </h1>
+      <p className="mt-2 text-muted-foreground">
+        Manage lockers and their status.
+      </p>
+
+      <div className="mt-8 flex flex-col gap-8 lg:flex-row"></div>
     </Layout>
   );
 }
--- admin-dashboard/src/routes/UserAdmin.tsx
+++ admin-dashboard/src/routes/UserAdmin.tsx
@@ -1,9 +1,52 @@
+import { UserMinus } from "lucide-react";
+
 import { Layout } from "@components";
+import { Button } from "@components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@components/ui/table";
 
 export function UserAdmin() {
   return (
     <Layout>
-      <h1>UserAdmin</h1>
+      <h1 className="text-[1.875rem] font-semibold leading-none">
+        User Administration
+      </h1>
+      <p className="mt-2 text-muted-foreground">
+        Manage your users and their subscriptions.
+      </p>
+
+      <div className="mt-8 flex flex-col gap-8 lg:flex-row">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Subscription</TableHead>
+              <TableHead>Subscription end date</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">Jimi</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Pro</TableCell>
+              <TableCell>May 24, 2024</TableCell>
+              <TableCell className="flex gap-4 items-center justify-end">
+                <Button variant="ghost">
+                  <UserMinus className="w-5 h-5" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </Layout>
   );
 }
